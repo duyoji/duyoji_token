565c58e3f0b96c548a04c1d4c597504a705e2462
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2018-01-18 12:13:49 +0900

    Modified truffle.js to migrate from truffle to rinkeby.

--- truffle.js
+++ truffle.js
@@ -1,4 +1,20 @@
+// See <http://truffleframework.com/docs/advanced/configuration>
+// to customize your Truffle configuration!
+//
+// https://blog.abuiles.com/blog/2017/07/09/deploying-truffle-contracts-to-rinkeby/
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "127.0.0.1", // Connect to geth on the specified
+      port: 8545,
+      from: "0x577c7b0e4bf76fd1d79a218f9d6e9414ee070f1d", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
+    }
+  }
 };
